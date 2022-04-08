04371c655c49b88cbc3465320ebb8344ad207c64
XD <s20.rex9000@gmail.com>
2022-04-08 21:19:30 +0300

    Repos created

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+public class Project
+{
+    public struct Note
+    {
+        string userInitials;
+        string markAuto;
+        string workType;
+        string dateOfAcceptiom;
+        string price;
+        string status;
+    };
+    public static int Main()
+    {
+        Note[] userlist = new Note[100];
+        int numberOfNotes = FillUserNotesReturnsNumberOfNotes(userlist);
+
+        return 0;
+    }
+
+    public static int FillUserNotesReturnsNumberOfNotes(Note[] userList)
+    {
+        //ifstream fin("ClientsNotesInput.txt", ios_base::in);
+        string s = File.ReadAllText("UserNoteInput.txt");
+        Console.WriteLine(s);
+        return 0;
+        //    int i = 0;
+        //    while (!fin.eof())
+        //    {
+        //        fin >> userList[i].userInitials >> userList[i].markAuto
+        //            >> userList[i].workType >> userList[i].dateOfAcceptiom
+        //            >> userList[i].price >> userList[i].status;
+        //        ++i;
+        //    }
+
+        //    fin.close();
+        //    return i + 1;
+        //}
+    }
+}
