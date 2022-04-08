1202ffcf1d76b3f1b4e4693db6d2779e16472069
XD <s20.rex9000@gmail.com>
2022-04-08 22:46:07 +0300

    added file-reading method

--- Program.cs
+++ Program.cs
@@ -5,38 +5,34 @@ public class Project
 {
     public struct Note
     {
-        string userInitials;
-        string markAuto;
-        string workType;
-        string dateOfAcceptiom;
-        string price;
-        string status;
+        public string userInitials,
+            markAuto,
+            workType,
+            dateOfAcceptiom,
+            price, 
+            status;
     };
+
     public static int Main()
     {
         Note[] userlist = new Note[100];
         int numberOfNotes = FillUserNotesReturnsNumberOfNotes(userlist);
-
         return 0;
     }
 
     public static int FillUserNotesReturnsNumberOfNotes(Note[] userList)
     {
-        //ifstream fin("ClientsNotesInput.txt", ios_base::in);
-        string s = File.ReadAllText("UserNoteInput.txt");
-        Console.WriteLine(s);
-        return 0;
-        //    int i = 0;
-        //    while (!fin.eof())
-        //    {
-        //        fin >> userList[i].userInitials >> userList[i].markAuto
-        //            >> userList[i].workType >> userList[i].dateOfAcceptiom
-        //            >> userList[i].price >> userList[i].status;
-        //        ++i;
-        //    }
+        string s = File.ReadAllText("UserNotesInput.txt");
+        // Console.WriteLine(s);
+        int i = 0;
+        while (s[i] != '.')
+        {
+            ++i;
+        }
 
-        //    fin.close();
-        //    return i + 1;
-        //}
+        StreamWriter print = new StreamWriter("UserNotesOutput.txt", true);
+        print.Write(s[0..i]);
+
+        return 0;        
     }
 }
